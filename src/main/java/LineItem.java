717c69659936612857749823937751edfe937e0d
Julius Lee <7julius.lee@gmail.com>
2021-09-10 19:31:59 -0700

    added salestax and regex for LineItem class

--- src/main/java/LineItem.java
+++ src/main/java/LineItem.java
@@ -2,14 +2,19 @@ public class LineItem {
     private final int quantity;
     private final String itemName;
     private final double unitPrice;
-    private boolean imported = false;
+    private int salesTaxRateInPercent = 10; // Sales Tax of 10% = 10
 
     public LineItem(int quantity, String itemName, double unitPrice) {
         this.quantity = quantity;
         this.itemName = itemName;
         this.unitPrice = unitPrice;
         if (itemName.contains("imported")) {
-            this.imported = true;
+            this.salesTaxRateInPercent = 5;
+        } else {
+            if (itemName.matches(".*(book|chocolate|pills).*")) {
+                System.out.println("matched");
+                this.salesTaxRateInPercent = 0;
+            }
         }
     }
 
@@ -18,12 +23,17 @@ public class LineItem {
         return quantity + " " + itemName + ": " + quantity * unitPrice;
     }
 
+    public double getSalesTax() {
+        return Math.round(quantity * unitPrice * salesTaxRateInPercent / 100 * 20) / 20d;
+    }
+
     public static void main(String[] args) {
-        LineItem item1 = new LineItem(1, "imported book", 12.49);
+        LineItem item1 = new LineItem(1, "book", 12.49);
         LineItem item2 = new LineItem(1, "music CD", 14.99);
         LineItem item3 = new LineItem(1, "chocolate bar", 0.85);
         System.out.println(item1);
         System.out.println(item2);
         System.out.println(item3);
+        System.out.println(item1.getSalesTax() + item2.getSalesTax() + item3.getSalesTax());
     }
 }
