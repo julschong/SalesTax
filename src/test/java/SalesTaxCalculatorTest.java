65940f4195c462a50e2c78448812db40e161b90f
Julius Lee <7julius.lee@gmail.com>
2021-09-12 01:11:09 -0700

    working on Calculator Test

---
+++ src/test/java/SalesTaxCalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SalesTaxCalculatorTest {
+
+    SalesTaxCalculator salesTaxCalculator;
+
+    @BeforeEach
+    void setUp() {
+        salesTaxCalculator = new SalesTaxCalculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void start() {
+
+
+    }
+}
